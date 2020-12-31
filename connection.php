e6f71c1c9347bfd4396032488710df06256af7da
Nithya-07 <61047319+Nithya-07@users.noreply.github.com>
2020-12-31 16:30:42 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -4,3 +4,31 @@ $dbhost = "localhost";
 $dbuser = "root";
 $dbpass = "";
 $dbname = "login_sample_db";
+if($con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
+{
+
+	die("failed to connect!");
+}
+else
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My website</title>
+</head>
+<body>
+
+	<a href="logout.php">Logout</a>
+	<h1>Successfully Logged in!</h1>
+	Welcome <?php echo $user_data['user_name']; ?>
+	<form>
+		<label for="age">Age:
+		</label><br>
+		<input type="text" id="age" name="Age"><br>
+		<label for="Contact Number">Contact Number:
+		</label><br>
+		<input type="text" id="Contact Number" name="Contact Number"><br>
+		<input type="submit" value="submit">
+	</form>
+
+</body>
+</html>
