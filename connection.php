7eaf70b80ed051c7337f99b9a8007cb795af1135
Nithya-07 <61047319+Nithya-07@users.noreply.github.com>
2020-12-31 16:18:49 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -4,9 +4,3 @@ $dbhost = "localhost";
 $dbuser = "root";
 $dbpass = "";
 $dbname = "login_sample_db";
-
-if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
-{
-
-	die("failed to connect!");
-}
